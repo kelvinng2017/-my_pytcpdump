777ea74fa825b486ac15b27b8d9ddddc3836d1f2
kelvinng2017 <kelvinngshuhui@gmail.com>
2021-10-10 19:02:00 +0800

    renew

--- my_main.py
+++ my_main.py
@@ -1,10 +1,12 @@
 import socket
+import codecs
 print("hello")
 s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
 while True:
     packet, _ = s.recvfrom(65565)
     print(packet)
     print(type(packet))  # hi
-    packet_decode = packet.decode('ascii')
+    #packet_decode = packet.decode('ascii')
+    packet_decode = codecs.encode(packet, 'hex')
     print(packet_decode)
     print(type(packet_decode))  # hi
