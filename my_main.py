e095b24e9090a38578676f53714556fe66186b43
kelvinng2017 <kelvinngshuhui@gmail.com>
2021-10-10 17:56:36 +0800

    renew

--- my_main.py
+++ my_main.py
@@ -3,4 +3,4 @@ s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
 while True:
     packet, _ = s.recvfrom(65565)
     print(packet)
-    print(type(packet))
+    print(type(packet))  # hi
