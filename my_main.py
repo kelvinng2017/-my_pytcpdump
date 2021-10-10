17d80c759fff5de5484d6f820ff77f0d032058f6
kelvinng2017 <kelvinngshuhui@gmail.com>
2021-10-10 22:11:26 +0800

    renew

--- my_main.py
+++ my_main.py
@@ -16,16 +16,17 @@ while True:
     # print(type(Eth_header_decode))
     # print(len(Eth_header_decode))
     Eth_header_decode_string = Eth_header_decode.decode("utf-8")
-    print(f"Eth_header_from:{Eth_header_decode_string[0:2]}:{Eth_header_decode_string[2:4]}:{Eth_header_decode_string[4:6]}:{Eth_header_decode_string[6:8]}:{Eth_header_decode_string[8:10]}:{Eth_header_decode_string[10:12]}")
-    print(f"Eth_header_to:{Eth_header_decode_string[12:14]}:{Eth_header_decode_string[14:16]}:{Eth_header_decode_string[16:18]}:{Eth_header_decode_string[18:20]}:{Eth_header_decode_string[20:22]}:{Eth_header_decode_string[22:24]}")
+    print(
+        f"Eth_header_from:{Eth_header_decode_string[0:2]}:{Eth_header_decode_string[2:4]}:{Eth_header_decode_string[4:6]}:{Eth_header_decode_string[6:8]}:{Eth_header_decode_string[8:10]}:{Eth_header_decode_string[10:12]}")
+    print(
+        f"Eth_header_to:{Eth_header_decode_string[12:14]}:{Eth_header_decode_string[14:16]}:{Eth_header_decode_string[16:18]}:{Eth_header_decode_string[18:20]}:{Eth_header_decode_string[20:22]}:{Eth_header_decode_string[22:24]}")
     print(f"EtherType:{Eth_header_decode_string[24:28]}")
     # print(Eth_header_decode)
     Ip_header = packet[14:34]
     Ip_header_decode = codecs.encode(Ip_header, 'hex')
     iph = struct.unpack('!BBHHHBBH4s4s', Ip_header)
     IHL_VERSION, TYPE_OF_SERVICE, total_len, pktID, FRAGMENT_STATUS, TIME_TO_LIVE, PROTOCOL, check_sum_of_hdr, src_IP, dest_IP = iph
-    src_IP_hex = codecs.encode(src_IP, 'hex')
-    #scr_IP_dec = int(src_IP_hex, 16)
+    scr_IP_dec = int(src_IP_hex, 16)
     print(f"IHL_VERSION:{IHL_VERSION}")
     print(f"TYPE_OF_SERVICE:{TYPE_OF_SERVICE}")
     print(f"total_len:{total_len}")
@@ -34,7 +35,7 @@ while True:
     print(f"TIME_TO_LIVE:{TIME_TO_LIVE}")
     print(f"PROTOCOL:{PROTOCOL}")
     print(f"check_sum_of_hdr:{check_sum_of_hdr}")
-    print(f"src_IP:{src_IP_hex}")
-    print(f"dest_IP:{dest_IP}")
+    print(f"src_IP:{socket.inet_ntoa(src_IP)}")
+    print(f"dest_IP:{socket.inet_ntoa(dest_IP)}")
     # print("Ip_header_decode")
     # print(Ip_header_decode)
--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-mac_add = "242ac1300020242ac1300030800"
-print(mac_add[2:4])
+import codecs
+mac_add = ("d\x95\xe2\xff").encode('utf-8')
+src_IP_hex = codecs.encode(mac_add, 'hex')
+print(int(src_IP_hex[0:2], 16))
+print(int(src_IP_hex[2:4], 16))
+print(int(src_IP_hex[4:6], 16))
+print(int(src_IP_hex[6:8], 16))
