832b19e3ecf3173388f1854d8b24c194d9254142
kelvinng2017 <kelvinngshuhui@gmail.com>
2021-10-10 19:53:25 +0800

    renew

--- my_main.py
+++ my_main.py
@@ -1,5 +1,6 @@
 import socket
 import codecs
+import struct
 print("hello")
 s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
 while True:
@@ -16,10 +17,11 @@ while True:
     # print(len(Eth_header_decode))
     Eth_header_decode_string = Eth_header_decode.decode("utf-8")
     print(f"Eth_header_from:{Eth_header_decode_string[0:2]}:{Eth_header_decode_string[2:4]}:{Eth_header_decode_string[4:6]}:{Eth_header_decode_string[6:8]}:{Eth_header_decode_string[8:10]}:{Eth_header_decode_string[10:12]}")
-    print(f"Eth_header_from:{Eth_header_decode_string[12:14]}:{Eth_header_decode_string[14:16]}:{Eth_header_decode_string[16:18]}:{Eth_header_decode_string[18:20]}:{Eth_header_decode_string[20:22]}:{Eth_header_decode_string[22:24]}")
+    print(f"Eth_header_to:{Eth_header_decode_string[12:14]}:{Eth_header_decode_string[14:16]}:{Eth_header_decode_string[16:18]}:{Eth_header_decode_string[18:20]}:{Eth_header_decode_string[20:22]}:{Eth_header_decode_string[22:24]}")
     print(f"EtherType:{Eth_header_decode_string[24:28]}")
     # print(Eth_header_decode)
     Ip_header = packet[14:34]
     Ip_header_decode = codecs.encode(Ip_header, 'hex')
+    print(struct.unpack('!BBHHHBBH4s4s', Ip_header))
     # print("Ip_header_decode")
     # print(Ip_header_decode)
